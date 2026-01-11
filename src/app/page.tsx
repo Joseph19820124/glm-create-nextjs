a04c7c640f0fb6f921cb58bfc96a98a662cacba6
Joseph19820124 <164839249+Joseph19820124@users.noreply.github.com>
2026-01-11 00:31:25 -0800

    Add Nike icon to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <main>
+      <svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 70 L45 30 L90 50" stroke="black" strokeWidth="8" fill="none" strokeLinecap="round" strokeLinejoin="round" />
+      </svg>
       <h1>Next.js App</h1>
       <p>This is a placeholder page.</p>
     </main>
